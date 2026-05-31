0a233abcd0ba88e43f07a3ff6b20dfd7b042a86a
Kartik Maheshwari... <kartikmaheshwari2011@gmail.com>
2026-05-31 22:02:21 +0530

    Create AdminPanel.java

---
+++ AdminPanel.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class AdminPanel extends JPanel {
+    public AdminPanel(ElectionMain parent) {
+        setBackground(Color.WHITE);
+        JButton back = new JButton("Back to Login");
+        back.addActionListener(e -> ((CardLayout)parent.getContentPane().getComponent(0).getLayout()).show(parent.getContentPane().getComponent(0), "AUTH"));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("Arial", 1, 18));
+        int y = 50;
+        for (var pos : ElectionData.voteTally.entrySet()) {
+            g.drawString(pos.getKey(), 50, y);
+            y += 30;
+            for (var cand : pos.getValue().entrySet()) {
+                g.fillRect(50, y, cand.getValue() * 20, 20);
+                g.drawString(cand.getKey() + " (" + cand.getValue() + ")", 60 + (cand.getValue() * 20), y + 15);
+                y += 30;
+            }
+            y += 20;
+        }
+    }
+}
